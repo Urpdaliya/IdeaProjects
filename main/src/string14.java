7441e047845e944aab8078b3df6f6ef03af1cb4c
Urpdaliya <114207299+Urpdaliya@users.noreply.github.com>
2023-02-03 21:34:05 +0530

    Create string14.java

---
+++ main/src/string14.java
@@ -0,0 +1,7 @@
+public class string14
+{
+    public static void main(String args[]){
+        String joinString1=String.join("-","welcome","to","java");
+        System.out.println(joinString1);
+    }
+}
