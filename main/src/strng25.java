d146f0d98bf45d02e2dbda6f02808b2984274b40
Urpdaliya <114207299+Urpdaliya@users.noreply.github.com>
2023-02-03 21:31:09 +0530

    Create strng25.java

---
+++ main/src/strng25.java
@@ -0,0 +1,8 @@
+public class strng25
+{
+    public static void main(String args[]){
+        String s1="  hello   ";
+        System.out.println(s1+"utsav");//without trim()
+        System.out.println(s1.trim()+"utsav");//with trim()
+    }
+}
