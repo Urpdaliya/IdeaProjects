19618bcc774d44986a401c627f52047b530a21a5
Urpdaliya <114207299+Urpdaliya@users.noreply.github.com>
2023-02-03 16:04:49 +0530

    Create test6.java

---
+++ main/src/test6.java
@@ -0,0 +1,9 @@
+public class test6
+{
+    public static void main(String args[]){
+        String s="Utsavpadaliya";
+        System.out.println("Original String: " + s);
+        System.out.println("Substring starting from index 6: " +s.substring(5));//Tendulkar
+        System.out.println("Substring starting from index 0 to 6: "+s.substring(0,5)); //Sachin
+    }
+}
