9dde4f10c8f74638949e991b78182762e9face4c
Urpdaliya <114207299+Urpdaliya@users.noreply.github.com>
2023-02-03 21:32:06 +0530

    Create string5.java

---
+++ main/src/string5.java
@@ -0,0 +1,6 @@
+public class string5{
+    public static void main(String args[]){
+        String s1="utsav";
+        System.out.println(s1.endsWith("v"));
+        System.out.println(s1.endsWith("u"));
+    }}
