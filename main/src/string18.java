795995de21f0346976ff84e2b61e35add8a6570a
Urpdaliya <114207299+Urpdaliya@users.noreply.github.com>
2023-02-03 21:33:52 +0530

    Create string18.java

---
+++ main/src/string18.java
@@ -0,0 +1,8 @@
+public class string18
+{
+    public static void main(String args[]){
+        String s1="My name is utsav. My name is nikunj. My name is mosin.";
+        String replaceString=s1.replaceAll("is","was");//replaces all occurrences of "is" to "was"
+        System.out.println(replaceString);
+    }
+}
