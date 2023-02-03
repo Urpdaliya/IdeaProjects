29ce91e70e288804a997b1f995241cddb94f78c3
Urpdaliya <114207299+Urpdaliya@users.noreply.github.com>
2023-02-03 20:56:20 +0530

    Create string1.java

---
+++ main/src/string1.java
@@ -0,0 +1,8 @@
+public class string1
+{
+    public static void main(String args[]){
+        String name="utsav";
+        char ch=name.charAt(4);//returns the char value at the 4th index
+        System.out.println(ch);
+    }
+}
