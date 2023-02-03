f44d0d4a8452a40c7678985ffc6a79f3523722d2
Urpdaliya <114207299+Urpdaliya@users.noreply.github.com>
2023-02-03 21:34:17 +0530

    Create string10.java

---
+++ main/src/string10.java
@@ -0,0 +1,9 @@
+public class string10{
+    public static void main(String args[]){
+        String str = new String("hello utsav how r u");
+        char[] ch = new char[10];
+        try{
+            str.getChars(6, 16, ch, 0);
+            System.out.println(ch);
+        }catch(Exception ex){System.out.println(ex);}
+    }}
